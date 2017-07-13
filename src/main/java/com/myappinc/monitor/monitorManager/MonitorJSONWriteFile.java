ee70a2a5a49b41ccd0cb9b5d9194455fc9906c5a
Huucc <huu@Huus-MNN.local>
2017-07-13 17:30:55 +0700

    Read and write to array

--- src/main/java/com/myappinc/monitor/monitorManager/MonitorJSONWriteFile.java
+++ src/main/java/com/myappinc/monitor/monitorManager/MonitorJSONWriteFile.java
@@ -6,53 +6,93 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.google.gson.JsonArray;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.io.IOException;
 import java.io.Reader;
+import java.sql.Array;
 
 
 public class MonitorJSONWriteFile {
   public void writeFile(String strLink) throws IOException {
-	  JSONObject obj = new JSONObject();
-		obj.put("link", strLink);
-		
-		// try-with-resources statement based on post comment below :)
-		try (FileWriter file = new FileWriter("/Users/huu/file1.txt")) {
-			file.write(obj.toJSONString());
-			System.out.println("Successfully Copied JSON Object to File...");
-			System.out.println("\nJSON Object: " + obj);
-		}
+	  JSONArray arr = new JSONArray();
+	  JSONObject json=new JSONObject();
+	  MonitorJSONWriteFile monitor = new MonitorJSONWriteFile();
+	  arr = monitor.getDataFile();
+	  
+	  if(arr != null) {
+		  json.put("name", "test");
+	      json.put("url",strLink);
+	      arr.add(json.toJSONString());
+	      
+	      try (FileWriter file = new FileWriter("/Users/huu/file1.txt")) {
+				file.write(arr.toString());
+				System.out.println("Successfully Copied JSON Object to File...");
+				System.out.println("\nJSON Object 12: " + arr);
+			}
+	  } else {
+	      HashMap<String, JSONObject> map = new HashMap<String, JSONObject>();
+	      for(int i = 0 ; i < 2 ; i++) {
+	        json.put("name",i);
+	        json.put("url",strLink);
+	        map.put("json", json);
+	        arr.add(json);
+			try (FileWriter file = new FileWriter("/Users/huu/file1.txt")) {
+				file.write(arr.toJSONString());
+				System.out.println("Successfully Copied JSON Object to File...");
+				System.out.println("\nJSON Object: " + arr);
+			}
+	      }
+	      System.out.println(arr);
+	  }      
+      
 	}
 	  
-  public String readFile() throws IOException {
+  public ArrayList<String> readFile() throws IOException {
 	  JSONParser parser = new JSONParser();
-	  String linkName = null;
-	  
-      try {
-    	  	//Reader reader = new FileReader("/Users/huu/file1.txt");
-    	  	BufferedReader reader = new BufferedReader(new FileReader("/Users/huu/file1.txt"));
-    	  	if(reader.readLine() != null) {
-    	  		Object obj = parser.parse(reader);
-    	          if(obj != null) {
-    	        	  	JSONObject jsonObject = (JSONObject) obj;
-    	              linkName = (String) jsonObject.get("link");
-    	          }
-    	  	}
-          
-        		  
-        
-      } catch (FileNotFoundException e) {
-          e.printStackTrace();
-      } catch (IOException e) {
-          e.printStackTrace();
-      } catch (ParseException e) {
-          e.printStackTrace();
-      }
-      return linkName;
+	  String linkName =null;
+	  String  url = null;
+	  ArrayList arrUrl = new ArrayList();
+	  Object obj = null;
+	try {
+		obj = parser.parse(new FileReader("/Users/huu/file1.txt"));
+	} catch (ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+      JSONArray jsonArray = (JSONArray) obj;
+
+      for (int i = 0; i < jsonArray.size(); i++) {
+
+          JSONObject jsonObjectRow = (JSONObject) jsonArray.get(i);
+          //linkName = (String) jsonObjectRow.get("link");
+           url = (String) jsonObjectRow.get("url");
+           arrUrl.add(url);
+     
   }
+      return arrUrl;
+}
   
+  public JSONArray getDataFile() throws IOException {
+	  JSONParser parser = new JSONParser();
+	  JSONArray jsonObject = new JSONArray();
+	  String linkName =null;
+	  String  url = null;
+	  Object obj = null;
+	  
+		try {
+			obj = parser.parse(new FileReader("/Users/huu/file1.txt"));
+			jsonObject = (JSONArray) obj;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return jsonObject;
+  }
 }
 
