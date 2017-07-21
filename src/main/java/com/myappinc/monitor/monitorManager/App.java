b3367cca9a74768430c4fabf467c5479ad384d66
Huucc <huu@Huus-MNN.local>
2017-07-21 11:20:06 +0700

    Config file

--- src/main/java/com/myappinc/monitor/monitorManager/App.java
+++ src/main/java/com/myappinc/monitor/monitorManager/App.java
@@ -2,6 +2,7 @@ package com.myappinc.monitor.monitorManager;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.spikeify.ffmpeg.FFmpeg;
 import com.spikeify.ffmpeg.FFmpegExecutor;
@@ -16,50 +17,79 @@ import chrriis.dj.nativeswing.swtimpl.NativeInterface;
 
 import javax.media.bean.playerbean.MediaPlayer;
 
-
-
 /**
  * Hello world!
  *
  */
-public class App 
-{   
+public class App {
 	String b;
-	MediaPlayer player;
-	
-	public void getb(String s){
-		this.b =s;
+	String a;
+	String c;
+	String linkFFmpeg;
+	String linkFFprobe;
+
+	public void getb(String s) {
+		this.b = s;
 	}
-	
-    public boolean runMonitor(String linkAddress) throws IOException
-    { 
-    	   	
-    	    String s = "http://origin.onworldtv.com:1935/live/sbtn/index.m3u8";
-        
- 
-       // System.out.println( "Hello World!" );
-        FFmpeg ffmpeg = new FFmpeg("/usr/local/Cellar/ffmpeg/3.3.2/bin/ffmpeg");
-        FFprobe ffprobe = new FFprobe("/usr/local/Cellar/ffmpeg/3.3.2/bin/ffprobe");
-        FFmpegProbeResult probeResult = ffprobe.probe(linkAddress);
-        FFmpegFormat format = probeResult.getFormat();
-        if(format.start_time > 0) {
-        		return true;
-        }
-        return false;
-//        System.out.format("%nFile: '%s' ; Format: '%s' ; Duration: %.3fs", 
-//        	format.filename, 
-//        	format.format_long_name,
-//        	format.duration
-//        );
-//
-//        FFmpegStream stream = probeResult.getStreams().get(0);
-//        System.out.format("%nCodec: '%s' ; Width: %dpx ; Height: %dpx",
-//        	stream.codec_long_name,
-//        	stream.width,
-//        	stream.height
-//        );
-      
-    }
-   
-   
+
+	public String getUrlFFmeg(String urlFFmpeg) {
+		this.a = urlFFmpeg;
+		return a;
+	}
+
+	public String getUrlFFprobe(String urlFFprobe) {
+		this.c = urlFFprobe;
+		return c;
+	}
+
+	public boolean runMonitor(String linkAddress) throws IOException {
+		App app = new App();
+		String s = "http://origin.onworldtv.com:1935/live/sbtn/index.m3u8";
+		String a = null;
+
+		// System.out.println( "Hello World!" );
+		// FFmpeg ffmpeg = new FFmpeg("/usr/local/Cellar/ffmpeg/3.3.2/bin/ffmpeg");
+		// FFprobe ffprobe = new FFprobe("/usr/local/Cellar/ffmpeg/3.3.2/bin/ffprobe");
+		FFmpeg ffmpeg = new FFmpeg(app.getUrlFFmeg(a));
+		FFprobe ffprobe = new FFprobe(app.getUrlFFprobe(c));
+		System.out.println(a + "a :");
+		FFmpegProbeResult probeResult = ffprobe.probe(linkAddress);
+		FFmpegFormat format = probeResult.getFormat();
+		if (format.start_time > 0) {
+			return true;
+		}
+		return false;
+		// System.out.format("%nFile: '%s' ; Format: '%s' ; Duration: %.3fs",
+		// format.filename,
+		// format.format_long_name,
+		// format.duration
+		// );
+		//
+		// FFmpegStream stream = probeResult.getStreams().get(0);
+		// System.out.format("%nCodec: '%s' ; Width: %dpx ; Height: %dpx",
+		// stream.codec_long_name,
+		// stream.width,
+		// stream.height
+		// );
+
+	}
+
+	public boolean runMonitor1(String linkAddress) throws IOException {
+
+		String s = "http://origin.onworldtv.com:1935/live/sbtn/index.m3u8";
+
+		MonitorJSONWriteFile monitor = new MonitorJSONWriteFile();
+		linkFFmpeg = monitor.readConfigFFmpeg();
+		linkFFprobe = monitor.readConfigFFmprobe();
+
+		FFmpeg ffmpeg = new FFmpeg(linkFFmpeg);
+		FFprobe ffprobe = new FFprobe(linkFFprobe);
+		FFmpegProbeResult probeResult = ffprobe.probe(linkAddress);
+		FFmpegFormat format = probeResult.getFormat();
+		if (format.start_time > 0) {
+			return true;
+		}
+		return false;
+	}
+
 }
--- src/main/java/com/myappinc/monitor/monitorManager/MonitorJSONWriteFile.java
+++ src/main/java/com/myappinc/monitor/monitorManager/MonitorJSONWriteFile.java
@@ -19,9 +19,13 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Array;
 
 public class MonitorJSONWriteFile {
+
 	public void writeFile(String strName, String strLink) throws IOException {
 		JSONArray arr = new JSONArray();
 		JSONObject json = new JSONObject();
@@ -33,8 +37,9 @@ public class MonitorJSONWriteFile {
 			json.put("url", strLink);
 			arr.add(json);
 
-			try (FileWriter file = new FileWriter("/Users/huu/file1.txt")) {
+			try (FileWriter file = new FileWriter(monitor.createFile())) {
 				file.write(arr.toJSONString());
+				System.out.println(" createfile :" + monitor.createFile());
 				System.out.println("Successfully Copied JSON Object to File...");
 				System.out.println("\nJSON Object 12: " + arr);
 			}
@@ -45,13 +50,42 @@ public class MonitorJSONWriteFile {
 			json.put("url", strLink);
 			map.put("json", json);
 			arr.add(json);
-			try (FileWriter file = new FileWriter("/Users/huu/file1.txt")) {
+			try (FileWriter file = new FileWriter(monitor.createFile())) {
 				file.write(arr.toJSONString());
+				System.out.println(" creatfile :" + monitor.createFile());
 				System.out.println("Successfully Copied JSON Object to File...");
 				System.out.println("\nJSON Object: " + arr);
 			}
 
 			System.out.println(arr);
+		}
+
+	}
+
+	public void writeConfigFile(String urlFFmpeg, String urlFFmprobe) throws IOException {
+		JSONArray arr = new JSONArray();
+		JSONObject json = new JSONObject();
+		MonitorJSONWriteFile monitor = new MonitorJSONWriteFile();
+		if (arr != null && arr.size() > 0) {
+			json.put("ffmprobeUrl", urlFFmprobe);
+			json.put("ffmpegUrl", urlFFmpeg);
+
+			try (FileWriter file = new FileWriter(monitor.createConfigFile())) {
+				file.write(json.toJSONString());
+				System.out.println(" createfile :" + monitor.createConfigFile());
+				System.out.println("Successfully Copied JSON Object to File...");
+				System.out.println("\nJSON Object 12: " + json.toJSONString());
+			}
+		} else {
+			json.put("ffmprobeUrl", urlFFmprobe);
+			json.put("ffmpegUrl", urlFFmpeg);
+			try (FileWriter file = new FileWriter(monitor.createConfigFile())) {
+				file.write(json.toJSONString());
+				System.out.println(" creatfile :" + monitor.createFile());
+				System.out.println("Successfully Copied JSON Object to File...");
+				System.out.println("\nJSON Object: " + json.toJSONString());
+			}
+
 		}
 
 	}
@@ -62,23 +96,24 @@ public class MonitorJSONWriteFile {
 		String url = null;
 		ArrayList arrUrl = new ArrayList();
 		Object obj = null;
-		File file = new File("/Users/huu/file1.txt");
-		if(!file.exists()) {
-			file.createNewFile();	
-		}
+
+		MonitorJSONWriteFile monitor = new MonitorJSONWriteFile();
+		String c = monitor.createFile();
+		
+
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("/Users/huu/file1.txt"));
-			
+			BufferedReader br = new BufferedReader(new FileReader(c));
+
 			if (br.readLine() != null) {
-			    obj = parser.parse(new FileReader("/Users/huu/file1.txt"));
+				obj = parser.parse(new FileReader(c));
 			}
-			
+
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		JSONArray jsonArray = (JSONArray) obj;
-		
-		if(jsonArray != null && jsonArray.size() > 0) {
+
+		if (jsonArray != null && jsonArray.size() > 0) {
 			for (int i = 0; i < jsonArray.size(); i++) {
 
 				JSONObject jsonObjectRow = (JSONObject) jsonArray.get(i);
@@ -88,32 +123,109 @@ public class MonitorJSONWriteFile {
 
 			}
 		}
-		
+
 		return arrUrl;
 	}
+
+	public String readConfigFFmpeg() throws IOException {
+		JSONParser parser = new JSONParser();
+		String linkName = null;
+		String urlFFmpeg = null;
+		ArrayList arrUrl = new ArrayList();
+		Object obj = null;
+
+		MonitorJSONWriteFile monitor = new MonitorJSONWriteFile();
+		String c = monitor.createConfigFile();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(c));
+
+			if (br.readLine() != null) {
+				obj = parser.parse(new FileReader(c));
+				JSONObject jsonObject = (JSONObject) obj;
+				urlFFmpeg = (String) jsonObject.get("ffmpegUrl");
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return urlFFmpeg;
+	}
+
+	public String readConfigFFmprobe() throws IOException {
+		JSONParser parser = new JSONParser();
+		String linkName = null;
+		String urlFFmprobe = null;
+		ArrayList arrUrl = new ArrayList();
+		Object obj = null;
+
+		MonitorJSONWriteFile monitor = new MonitorJSONWriteFile();
+		String c = monitor.createConfigFile();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(c));
+
+			if (br.readLine() != null) {
+				obj = parser.parse(new FileReader(c));
+				JSONObject jsonObject = (JSONObject) obj;
+				urlFFmprobe = (String) jsonObject.get("ffmprobeUrl");
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return urlFFmprobe;
+	}
+
+	public String createFile() throws IOException {
+		File dir = new File("tmp/test");
+		dir.mkdirs();
+		File tmp = new File(dir, "fileprocess.txt");
+		if (!tmp.exists()) {
+			tmp.createNewFile();
+		}
+
+		System.out.println("file name : " + tmp.getAbsolutePath());
+		return tmp.getAbsolutePath();
+
+	}
+
+	public String createConfigFile() throws IOException {
+		File dir = new File("tmp/test");
+		dir.mkdirs();
+		File tmp = new File(dir, "config.txt");
+		if (!tmp.exists()) {
+			tmp.createNewFile();
+		}
+
+		System.out.println("file name : " + tmp.getAbsolutePath());
+		return tmp.getAbsolutePath();
+
+	}
+
 	public ArrayList<String> readName() throws IOException {
 		JSONParser parser = new JSONParser();
 		String linkName = null;
 		String url = null;
 		ArrayList arrUrl = new ArrayList();
 		Object obj = null;
-		File file = new File("/Users/huu/file1.txt");
-		if(!file.exists()) {
-			file.createNewFile();	
-		}
+		MonitorJSONWriteFile monitor = new MonitorJSONWriteFile();
+		String c = monitor.createFile();
+
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("/Users/huu/file1.txt"));
-			
+
+			BufferedReader br = new BufferedReader(new FileReader(c));
+
 			if (br.readLine() != null) {
-			    obj = parser.parse(new FileReader("/Users/huu/file1.txt"));
+				obj = parser.parse(new FileReader(c));
 			}
-			
+
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		JSONArray jsonArray = (JSONArray) obj;
-		
-		if(jsonArray != null && jsonArray.size() > 0) {
+
+		if (jsonArray != null && jsonArray.size() > 0) {
 			for (int i = 0; i < jsonArray.size(); i++) {
 
 				JSONObject jsonObjectRow = (JSONObject) jsonArray.get(i);
@@ -123,11 +235,10 @@ public class MonitorJSONWriteFile {
 
 			}
 		}
-		
+
 		return arrUrl;
 	}
-	
-	
+
 	public void deleteJson(int index) throws FileNotFoundException, IOException {
 		MonitorJSONWriteFile monitor = new MonitorJSONWriteFile();
 		JSONParser parser = new JSONParser();
@@ -138,28 +249,28 @@ public class MonitorJSONWriteFile {
 		jsonObject = monitor.getDataFile();
 
 		try {
-			obj = parser.parse(new FileReader("/Users/huu/file1.txt"));
+			obj = parser.parse(new FileReader(monitor.createFile()));
 			jsonObject = (JSONArray) obj;
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		jsonObject.remove(index);
-		FileWriter file = new FileWriter("/Users/huu/file1.txt");
+		FileWriter file = new FileWriter(monitor.createFile());
 		file.write(jsonObject.toJSONString());
 		file.flush();
-		
-	} 
-	
+
+	}
 
 	public JSONArray getDataFile() throws IOException {
 		JSONParser parser = new JSONParser();
 		JSONArray jsonObject = new JSONArray();
 		String linkName = null;
+		MonitorJSONWriteFile monitor = new MonitorJSONWriteFile();
 		String url = null;
 		Object obj = null;
 
 		try {
-			obj = parser.parse(new FileReader("/Users/huu/file1.txt"));
+			obj = parser.parse(new FileReader(monitor.createFile()));
 			jsonObject = (JSONArray) obj;
 		} catch (ParseException e) {
 			e.printStackTrace();
